41a929700c6b7ed85d263c2a526cd96b5ea13dc1
raggazzi <raggazzi94@gmail.com>
2023-08-06 18:43:32 +0200

    initial commit

---
+++ src/main/java/com/gamerzdata/gamerzdata/GamerzdataApplication.java
@@ -0,0 +1,13 @@
+package com.gamerzdata.gamerzdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamerzdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamerzdataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gamerzdata/gamerzdata/GamerzdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamerzdata.gamerzdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamerzdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
